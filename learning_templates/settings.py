f1943a7bc87afb731de780387fa5fa8add54ea43
Rohit Kumar Das <rohitdas10595@gmail.com>
2020-07-02 18:53:03 +0530

    minor issues

--- learning_templates/settings.py
+++ learning_templates/settings.py
@@ -21,7 +21,7 @@ TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '96#4*d9r%6rrq!#4s$m4#ar_s0%)dgk@%@z4a7081&c&pdhgkf'
+SECRET_KEY = 'secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
